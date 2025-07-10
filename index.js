c21edd2e91917174dffa488abab3df5cf452e11b
jeikai <98737616+jeikai@users.noreply.github.com>
2025-07-10 11:20:02 +0700

    fix(index): fix port in index

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(5000, () => {
   console.log(`Server is running on port ${PORT}`);
 });
