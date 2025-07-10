07209fd05d3555f14873459e489934ae0d3015e9
jeikai <98737616+jeikai@users.noreply.github.com>
2025-07-10 15:07:14 +0700

    fix(host): Add password to DB password

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const pool = mysql.createPool({
     host: '172.17.0.1',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
+    password: process.env.DB_PASSWORD || '123456',
     database: process.env.DB_NAME || 'funbug',
     waitForConnections: true,
     connectionLimit: 10,
