3d6c4bb6ad47d5a9566285105e5a352aba3ee657
jeikai <98737616+jeikai@users.noreply.github.com>
2025-07-10 11:24:14 +0700

    fix(port): change port in deploy.yml

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(5000, () => {
   console.log(`Server is running on port ${PORT}`);
 });
